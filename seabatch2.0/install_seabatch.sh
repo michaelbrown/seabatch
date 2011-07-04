c5f063586182df07210038a2e827a152675eba24
Michael Brown <brown.michael.scott@gmail.com>
2011-07-04 16:16:53 -0300

    second commit

---
+++ seabatch2.0/install_seabatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+
+echo "Thank you for downloading SeaBatch 2.0!"
+echo ""
+echo "This script sets up the SeaBatch environment on your system. \
+(NOTE: SeaBatch currently only runs on systems using the BASH shell." | fold -s -w75
+echo ""
+echo -n "Would you like to continue (y/n)?"
+read VAR1
+echo $VAR1
+
