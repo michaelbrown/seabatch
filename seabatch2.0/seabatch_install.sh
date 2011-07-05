f86f9fcab377909426d88bb018818b62f53ee2a5
Michael Brown <brown.michael.scott@gmail.com>
2011-07-05 00:45:56 -0300

    removed seabatch_install.sh

--- seabatch2.0/seabatch_install.sh
+++
@@ -1,8 +0,0 @@
-echo 'Hello and thank you for downloading SeaBatch'
-
-SEABATCH=$PWD
-SEABATCH_ENV_FILE=${SEABATCH}'/sub/config/seabatch.env'
-
-echo 'export SEABATCH='${SEABATCH} > ${SEABATCH_ENV_FILE}
-echo 'alias seabatch=mkdir log; cp '${SEABATCH}'/master_file.txt log/processing_variables.txt;' ${SEABATCH}'/sub/bin/run_seabatch.sh | tee log/log.txt' >> ${SEABATCH_ENV_FILE}
-echo 'source' ${SEABATCH_ENV_FILE} >> ~/.bashrc
